2caae8d6cb85f49e6b50ac0e6116de659231e8cf
Bryan <blindaa121@gmail.com>
2022-08-30 17:51:18 -0700

    Add blog slider and custom styling

--- src/components/Medium/index.js
+++ src/components/Medium/index.js
@@ -1,71 +1,68 @@
 import Link from "@docusaurus/Link";
 import classnames from "classnames";
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import "./Slider.scss"
 import styles from "./styles.module.css";
 
-const ITEMS_PER_ROW = 3; // Sync up the item col width if this is changed.
-
 const blog_json = require("./blogs.json");
-
-const blogs = blog_json.items.slice(0, 6);
+const blogs = blog_json.items.slice(0, 9);
+const sliderSettings = {
+  autoplay: true,
+  autoplaySpeed: 7500,
+  dots: true,
+  infinite: true,
+  speed: 850,
+  slidesToShow: 3,
+  slidesToScroll: 3
+}
 
 function Medium() {
+  const BlogCard = ({ blog }) => {
+    return (
+      <div className={classnames("card", styles.showcaseBlog)}>
+        <div className="card__image">
+          <img src={blog.thumbnail} alt={blog.title} className={styles.blogImage} />
+        </div>
+        <div className="card__body">
+          <h3 className="avater__name">{blog.title}</h3>
+          <div className="avatar__intro margin-left--none">
+            <p className={classnames("text--secondary", styles.blogAuthor)}>
+              By: {blog.author}
+            </p>
+          </div>
+          <br />
+          <div className="avatar__intro margin-left--none">
+            <div className={styles.content}>
+              <div dangerouslySetInnerHTML={{__html: blog.content }} />
+            </div>
+          </div>
+        </div>
+        <div className="card__footer">
+          <Link
+            className="button button--outline button--primary button--block"
+            variant="plain"
+            href={blog.link}
+            target="_blank"
+            uppercase="false"
+          >
+            {" "}
+            Read More on Medium
+          </Link>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="container">
-      <div className="padding-top--lg">
-        {blogs && blogs.length && (
-          <div className="row">
-            {blogs.map((blog) => (
-              <div key={blog.title} className="col col--4 margin-bottom--lg">
-                <div className={classnames("card", styles.showcaseBlog)}>
-                  <div className="card__image">
-                    <img
-                      src={blog.thumbnail}
-                      alt={blog.title}
-                      className={styles.blogImage}
-                    />
-                  </div>
-                  <div className="card__body">
-                    <div className="avatar__intro margin-left--none">
-                      <h3 className="avater__name">{blog.title}</h3>
-                    </div>
-                    <div className="avatar__intro margin-left--none">
-                      <p
-                        className={classnames(
-                          "text--secondary",
-                          styles.blogAuthor
-                        )}
-                      >
-                        By: {blog.author}
-                      </p>
-                    </div>
-                    <br />
-                    <div className="avatar__intro margin-left--none">
-                      <div className={styles.content}>
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html: blog.content,
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="card__footer">
-                    <Link
-                      className="button button--outline button--primary button--block"
-                      variant="plain"
-                      href={blog.link}
-                      target="_blank"
-                      uppercase="false"
-                    >
-                      {" "}
-                      Read More on Medium
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
+      <div className="slider-container">
+        {blogs?.length && (
+          <Slider {...sliderSettings}>
+            {blogs.map((blog) => <BlogCard blog={blog} />)}
+          </Slider>
         )}
       </div>
     </div>
