4536739196286689a908972981c3bef718b884be
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-09-02 15:26:07 -0400

    Rename sidebar.js to sidebars.js

---
+++ docs/sidebars.js
@@ -0,0 +1,8 @@
+const sidebars =
+  (require("./cdss/sidebars"),
+  require("./expedition/sidebars"),
+  require("./panos/sidebars"),
+  require("./sase/sidebars"),
+  require("./cloudngfw/sidebars"));
+
+module.exports = { sidebars };
