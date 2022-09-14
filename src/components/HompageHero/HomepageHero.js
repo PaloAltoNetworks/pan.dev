6d43134ff91f80dff29250f9cc772f04dfd3f811
Bryan <blindaa121@gmail.com>
2022-09-14 16:49:59 -0700

    Move svg illustration to hero

--- src/components/HompageHero/HomepageHero.js
+++ src/components/HompageHero/HomepageHero.js
@@ -1,21 +1,20 @@
 import React from "react";
-import Link from "@docusaurus/Link";
-import classnames from "classnames";
 import './HomepageHero.scss'
 
 function HomepageHero() {
   return (
-    <div className="hero-container">
+    <div className="hero-container container">
       <header className="hero-content-container">
-        <div className="container hero-header-container">
+        <div className="hero-header-container">
           <h1 className="hero-header">
-            The home of <span className="hero-colored-text">developer docs </span> at..
+            The home of <span className="hero-colored-text">developer docs </span> at
           </h1>
         </div>
-        <div className="container hero-image-container">
-          <img className="hero-image" src="img/PANW_Parent_Logo_Black.svg"/>
+        <div className="hero-image-container">
+          <img className="hero-image__logo" src="img/PANW_Parent_Logo_Black.svg"/>
         </div>
       </header>
+      <img className="hero-image__illustration" src="img/homepage-hero/hero-1.svg"/>
     </div>
   )
 }
