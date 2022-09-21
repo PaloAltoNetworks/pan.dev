95e6b91e5c598dc2a88199d47e9ebe97045bde5e
Bryan <blindaa121@gmail.com>
2022-09-20 17:35:44 -0700

    Refactor featured cards with navbar items

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,97 +1,66 @@
-import useBaseUrl from "@docusaurus/useBaseUrl";
 import React from "react";
-import Button from "@theme/Button";
-import clsx from "clsx";
+import NavbarNavLink from "@theme/NavbarItem/NavbarNavLink";
 import { useColorMode } from '@docusaurus/theme-common'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import clsx from "clsx";
 import "./Featured.scss";
 
-const features = [
-  {
-    title: <>Cloud Native Security</>,
-    imageUrl: "/img/featured-card-logos/PrismaCloud_Light_Mode.svg",
-    darkImageUrl: "/img/featured-card-logos/PrismaCloud_Dark_Mode.svg",
-    toPage: "https://prisma.pan.dev",
-    target: "_self",
-    color: "prisma",
-    description: (
-      <p>
-        Discover the APIs, tools and techniques necessary for bringing DevOps
-        practices to the cloud.
-      </p>
-    ),
-  },
-  {
-    title: <>Secure Access Service Edge</>,
-    imageUrl: "/img/featured-card-logos/PrismaSASE_Light_Mode.svg",
-    darkImageUrl: "/img/featured-card-logos/PrismaSASE_Dark_Mode.svg",
-    toPage: "https://pan.dev/sase",
-    target: "_self",
-    color: "prisma",
-    description: (
-      <p>
-        Discover Prisma SASE APIs, including Prisma Access and Prisma SD-WAN.
-      </p>
-    ),
-  },
-  {
-    title: <>Security Operations</>,
-    imageUrl: "/img/featured-card-logos/CortexXSOAR_Light_Mode.svg",
-    darkImageUrl: "/img/featured-card-logos/CortexXSOAR_Dark_Mode.svg",
-    toPage: "https://xsoar.pan.dev",
-    color: "cortex",
-    description: (
-      <p>
-        Browse reference docs, tutorials, the XSOAR Marketplace and more.
-      </p>
-    ),
-  },
-  {
-    title: <>Network Security</>,
-    imageUrl: "/img/Strata_Logo.svg",
-    darkImageUrl: "/img/Strata_Logo.svg",
-    toPage: "https://strata.pan.dev",
-    color: "panos",
-    description: (
-      <p>
-        Learn how to make the most of the PAN-OS APIs, Expedition, Terraform,
-        Ansible, and more.
-      </p>
-    )
-  },
-];
-
-function Feature({ imageUrl, title, description, toPage, color, darkImageUrl }) {
+function FeaturedCard({ colorclass, description, label, products }) {
   const { colorMode } = useColorMode();
-  const imgUrl = colorMode === "light" ? useBaseUrl(imageUrl) : useBaseUrl(darkImageUrl);
-  const toUrl = toPage ? useBaseUrl(toPage) : null;
+
+  function ProductContent({ product }) {
+    const { apiDocs, docs, label } = product; 
+    const allDocs = [...docs, ...apiDocs]
+
+    return (
+      <div className="featured-card__product-container">
+        <h3 className="featured-card__product-group-label">{label}</h3>
+        <ul className="featured-card__product-list">
+          {
+            allDocs.map(docs => {
+              const { label, to, icon } = docs
+              const iconSrc = icon === "doc"
+                ? `/icons/docs-icon-${colorMode === "light" ? "dark" : "light"}.svg`
+                : `/icons/api-docs-icon-${colorMode === "light" ? "dark" : "light"}.svg`
+
+              return <li className="featured-card__product-list-item">
+                <div className="product-icon-container">
+                  <img src={iconSrc}/>
+                </div>
+                <NavbarNavLink label={label} to={to}/>
+              </li>
+            })
+          }
+        </ul>
+      </div>
+    )
+  }
+
   return (
-    <div className={clsx("featured-card-container", color)}>
-      <span></span>
+    <div className={clsx("featured-card-container", colorclass)}>
       <div className="featured-card-content">
-        <img className="featured-card-image" src={imgUrl}/>
-        <h2>{title}</h2>
-        {description}
-        <Button
-          variant="plain"
-          href={toUrl}
-          target="_self"
-          uppercase={false}
-          newTab={false}
-        >
-          Learn More
-        </Button>
+        <h2 className="featured-card-content__label">{label}</h2>
+        <div className="featured-card-content__section-divider"/>
+        <p className="featured-card-content__description">{description}</p>
+        <div className="featured-card__products-container">
+          {products.map((product, i) => <ProductContent key={i} product={product}/>)}
+        </div>
       </div>
     </div>
   );
 }
 
-function Featured() {
+function FeaturedCardIndex() {
+  const { siteConfig: { themeConfig } } = useDocusaurusContext();
+  const { navbar: { items } } = themeConfig;
+  const featuredCards = items[0].items;
+
   return (
     <div className="featured-container container">
-      {features && features.length && (
+      {featuredCards?.length && (
         <div className="featured-cards-container">
-          {features.map((props, idx) => (
-            <Feature key={idx} {...props} />
+          {featuredCards.map((props, idx) => (
+            <FeaturedCard key={idx} {...props} />
           ))}
         </div>
       )}
@@ -99,4 +68,4 @@ function Featured() {
   );
 }
 
-export default Featured;
+export default FeaturedCardIndex;
