c0bd0313615022c4f37a32e7f186ecadbfb1e4b7
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2022-09-27 21:31:57 -0400

    Support selective loading of content and refactor codebase (#149)
    * initial split
    
    * autogeneration update
    
    * Refactor sidebars to dynamically resolve product sidebars
    
    * Rename and refactor all product sidebars
    
    * Re-gen API docs
    
    * Cleanup docusaurus config
    
    * Upgrade to v1.2.1 and add additional scripts
    
    * Rename DOCS_PLUGIN_INCLUDE to PRODUCTS_INCLUDE
    
    Co-authored-by: Charlie Sestito <csestito@paloaltonetworks.com>

--- docs/sidebars.js
+++
@@ -1,8 +0,0 @@
-module.exports = Object.assign(
-  {},
-  require("./cdss/sidebars"),
-  require("./expedition/sidebars"),
-  require("./panos/sidebars"),
-  require("./sase/sidebars"),
-  require("./cloudngfw/sidebars")
-)
