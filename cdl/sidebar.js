4575ec87cae48540c8a77c5cd40a94b692925603
Roy Thompson <rthompson@paloaltonetworks.com>
2022-10-05 19:46:16 +0200

    Update sidebar.js

--- cdl/sidebar.js
+++ cdl/sidebar.js
@@ -4,6 +4,10 @@ module.exports = {
       type: "doc",
       id: "logforwarding/docs/home",
     },
+	{
+		type: "doc",
+		id: "logforwarding/docs/getstarted_lf"
+	},
   ],
   logging_service_api: require("../static/cdl/logforwarding/spec/sidebar_builder").sidebar,
 };
