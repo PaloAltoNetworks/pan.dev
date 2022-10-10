a0d245c97b4a8785f2bd01e8eff299c46bcd333f
Roy Thompson <rthompson@paloaltonetworks.com>
2022-10-10 16:06:57 +0200

    DOCS-1196

--- cdl/sidebar.js
+++ cdl/sidebar.js
@@ -8,6 +8,18 @@ module.exports = {
 		type: "doc",
 		id: "logforwarding/docs/getstarted_lf"
 	},
+	{
+		type: "doc",
+		id: "logforwarding/docs/specifylogs_lf"
+	},	
+	{
+		type: "doc",
+		id: "logforwarding/docs/syslog_lf"
+	},
+	{
+		type: "doc",
+		id: "logforwarding/docs/https_lf"
+	}
   ],
   logging_service_api: require("../static/cdl/logforwarding/spec/sidebar_builder").sidebar,
 };
