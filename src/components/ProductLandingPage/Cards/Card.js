8e1372cc9531f1ead1b2502234c3d400ac1747b6
Bryan <blindaa121@gmail.com>
2022-11-17 23:27:09 -0800

    Create Card component and styles

---
+++ src/components/ProductLandingPage/Cards/Card.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import Link from '@docusaurus/Link'
+import './Card.scss'
+
+function Card({ cta, description, image, title, type, links, logo }) {
+  const CardFooterCTA = ({ cta }) => {
+    const { content, type } = cta
+
+    if (type === 'single') {
+      if (content.logoSrc) {
+        return (
+          <Link className="card-cta__single-link" href={content.link}>
+            <img className="card-cta__logo" src={content.logoSrc} alt={content.logoAlt} />
+          </Link>
+        )
+      }
+
+      return (
+        <Link className="card-cta__single-link" href={content.link}>
+          {content.text}
+        </Link>
+      )
+    }
+
+    if (type == 'double') {
+      return (
+        <div className="card-cta-container">
+          {content.map(ctaInfo => {
+            if (ctaInfo.logoSrc) {
+              return (
+                <Link
+                  className="card-cta__logo-link"
+                  href={ctaInfo.link}
+                >
+                  <img className="card-cta__logo" src={ctaInfo.logoSrc} />
+                </Link>
+              )
+            } else {
+              return (
+                <Link className="card-cta__docs-link" href={ctaInfo.link}>
+                  <span className="card-cta__text">{ctaInfo.text}</span>
+                  <img src='img/icons/arrow-forward.svg' />
+                </Link>
+              )
+            }
+          })}
+        </div>
+      )
+    }
+  }
+
+  const ImageCard = ({ cta, description, image, title }) => {
+    return (
+      <div class="image-card-container">
+        <div class="image-card__image">
+          <img src={image.src} alt={image.alt} />
+        </div>
+        <div class="image-card__body">
+          <h4>{title}</h4>
+          <p>{description}</p>
+          <CardFooterCTA cta={cta} />
+        </div>
+      </div>
+    )
+  }
+
+  const InfoCard = ({ cta, description, title }) => {
+    return (
+      <div class="info-card-container">
+        <div class="info-card__body">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <CardFooterCTA cta={cta} />
+        </div>
+      </div>
+    )
+  }
+
+  const LinkCard = ({ cta, links, title }) => {
+    return (
+      <div class="link-card-container">
+        <div class="link-card__body">
+          <h3>{title}</h3>
+          <ul class="link-card__list">
+            {links.map(link => <li><Link href={link.path}>{link.text}</Link></li>)}
+          </ul>
+          <CardFooterCTA cta={cta} />
+        </div>
+      </div>
+    )
+  }
+
+  const renderCard = () => {
+    if (type === "imageCard") {
+      return (
+        <ImageCard
+          cta={cta}
+          description={description}
+          image={image}
+          title={title}
+        />
+      )
+    } else if (type === "linkCard") {
+      return (
+        <LinkCard title={title} links={links} cta={cta} />
+      )
+    } else {
+      return <InfoCard cta={cta} description={description} title={title} />
+    }
+  }
+
+  return (
+    <>{renderCard()}</>
+  )
+}
+
+export default Card
