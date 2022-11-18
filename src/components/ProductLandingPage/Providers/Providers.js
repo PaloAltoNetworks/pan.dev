76b15cdbba90ed1d3d0f66fe783baf342368b065
Bryan <blindaa121@gmail.com>
2022-11-17 23:28:01 -0800

    Create Providers component and styles

---
+++ src/components/ProductLandingPage/Providers/Providers.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from '../Cards/Card'
+import './Providers.scss'
+
+function Providers({ header, subheader, providerCards }) {
+  return (
+    <div className="providers-container">
+      <section className="container">
+        <header className="providers-header">
+          {header}
+          {subheader}
+        </header>
+        <div className="provider-cards-container">
+          {providerCards.map(card =>
+            <Card
+              cta={card.cta}
+              description={card.description}
+              title={card.title}
+              version={card.version}
+            />
+          )}
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Providers
