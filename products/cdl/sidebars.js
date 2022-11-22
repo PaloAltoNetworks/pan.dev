eb26f8dbd55bc9815bd5fb4a715e6497cddfc8b2
Steve Sarette <ssarette@paloaltonetworks.com>
2022-11-21 19:59:17 -0800

    cdl updates from october that never got published

--- products/cdl/sidebars.js
+++ products/cdl/sidebars.js
@@ -5,6 +5,20 @@ module.exports = {
     "cdl/docs/logforwarding/specifylogs_lf",
     "cdl/docs/logforwarding/syslog_lf",
     "cdl/docs/logforwarding/https_lf",
+    {
+      type: "category",
+      label: "Log Forwarding Release Notes",
+      items: [
+        {
+          type: "doc",
+          id: "cdl/docs/logforwarding/release-notes/changelog",
+        },
+        {
+          type: "doc",
+          id: "cdl/docs/logforwarding/release-notes/relnotes",
+        },
+            ],
+    },
   ],
   cdl_logforwarding_api: [
     "cdl/api/logforwarding/logforwardingapi",
