87d67a8d6513ccd7b068d58b26cfa5df2f97e994
Bryan <blindaa121@gmail.com>
2022-12-07 17:30:43 -0800

    Add ideal image plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -643,6 +643,16 @@ const config = {
         docItemComponent: "@theme/ApiItem",
       },
     ],
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
   ],
   stylesheets: [
     {
