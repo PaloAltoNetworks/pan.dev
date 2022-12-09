617f13552dcc3fea0b65c17278c44a5421e3875a
Bryan <blindaa121@gmail.com>
2022-12-08 21:35:33 -0800

    Add Medium blog and Tabs to feeds section

--- src/components/ProductLandingPage/Feeds/Feeds.js
+++ src/components/ProductLandingPage/Feeds/Feeds.js
@@ -1,45 +1,63 @@
-import React from 'react'
-import Link from '@docusaurus/Link'
-import jsonFeeds from './feeds.json'
-import './Feeds.scss'
+import React from 'react';
+import Link from '@docusaurus/Link';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+import feedsJson from './feeds.json';
+import mediumFeedsJson from '../../Medium/blogs.json';
+import './Feeds.scss';
 
 function Feeds() {
-  const imageFeeds = jsonFeeds.items.slice(0, 2);
-  const nonImageFeeds = jsonFeeds.items.slice(2, 6);
+  const hashicorpImageFeeds = feedsJson.items.slice(0, 2);
+  const hashicorpFeeds = feedsJson.items.slice(2, 6);
+  const mediumImageFeeds = mediumFeedsJson.items.slice(0, 2);
+  const mediumFeeds = mediumFeedsJson.items.slice(2, 6);
+
+  const FeedItem = ({ feeds, imageFeeds }) => {
+    return (
+      <div className="feeds-image-list-container">
+        <div className="feeds-image-container">
+          {imageFeeds.map((feed, i) =>
+            <Link key={i} to={feed.link}>
+              <div className="feeds-image-text-wrapper">
+                <img
+                  src="/img/product-landing/terraform/feeds/stock-feed.jpg"
+                  alt={feed.title}
+                />
+                <h3 className="feeds__title">{feed.title}</h3>
+              </div>
+            </Link>
+          )}
+        </div>
+        <ul className="feeds-list">
+          {feeds.map((feed, i) =>
+            <li key={i} className="feeds-list__item">
+              <Link to={feed.link}>{feed.title}</Link>
+              <img
+                className="external-icon"
+                src="/img/icons/external-icon.png"
+                alt="External icon"
+              />
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
 
   return (
     <div className="container">
       <section className="feeds-container">
         <header className="feeds-header">
-          <h1>Latest Terraform News from Hashicorp</h1>
+          <h1>Latest Terraform News from</h1>
         </header>
-        <div className="feeds-image-list-container">
-          <div className="feeds-image-container">
-            {imageFeeds.map((feed, i) =>
-              <Link key={i} to={feed.link}>
-                <div className="feeds-image-text-wrapper">
-                  <img
-                    src="/img/product-landing/terraform/feeds/stock-feed.jpg"
-                    alt={feed.title}
-                  />
-                  <h3 className="feeds__title">{feed.title}</h3>
-                </div>
-              </Link>
-            )}
-          </div>
-          <ul className="feeds-list">
-            {nonImageFeeds.map((feed, i) =>
-              <li key={i} className="feeds-list__item">
-                <Link to={feed.link}>{feed.title}</Link>
-                <img
-                  className="external-icon"
-                  src="/img/icons/external-icon.png"
-                  alt="External icon"
-                />
-              </li>
-            )}
-          </ul>
-        </div>
+        <Tabs>
+          <TabItem value="HashiCorp" label="HashiCorp">
+            <FeedItem feeds={hashicorpFeeds} imageFeeds={hashicorpImageFeeds} />
+          </TabItem>
+          <TabItem value="PAN.dev" label="PAN.dev">
+            <FeedItem feeds={mediumFeeds} imageFeeds={mediumImageFeeds} />
+          </TabItem>
+        </Tabs>
       </section>
     </div>
   )
