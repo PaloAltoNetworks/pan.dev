e0459a6829a6eab381c39385722f3366c733d526
Bryan <blindaa121@gmail.com>
2022-12-12 16:30:42 -0800

    Scale down hero tech award image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -646,9 +646,9 @@ const config = {
     [
       '@docusaurus/plugin-ideal-image',
       {
-        quality: 70,
-        max: 1030, // max resized image's size.
-        min: 640, // min resized image's size. if original is lower, use that size.
+        quality: 80,
+        max: 450, // max resized image's size.
+        min: 300, // min resized image's size. if original is lower, use that size.
         steps: 2, // the max number of images generated between min and max (inclusive)
         disableInDev: false,
       },
