0243efca35c06de0ff19d492327dd9b028931ce2
Rashid Ma <rmakhmudov@paloaltonetworks.com>
2022-12-19 17:47:34 -0600

    added sidebar for compute

---
+++ products/compute/sidebars.js
@@ -0,0 +1,12 @@
+module.exports = {
+  computeapi: [
+    {
+      type: "category",
+      label: "Compute Self-hosted",
+      collapsed: true,
+      items: [
+        require("api/sidebar")
+      ],
+    },
+  ],
+};
