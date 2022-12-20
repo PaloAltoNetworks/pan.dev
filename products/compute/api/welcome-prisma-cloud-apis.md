98c66dfd437de6e1450478a4002b7fc1867707a9
Rashid Ma <rmakhmudov@paloaltonetworks.com>
2022-12-19 21:53:08 -0600

    trying to create sidebar with API versions for compute

---
+++ products/compute/api/welcome-prisma-cloud-apis.md
@@ -0,0 +1,13 @@
+---
+id: compute-api-reference-home
+title: Welcome to the Compute APIs
+slug: /compute/api
+keywords:
+  - Developer
+  - Prisma
+  - Prisma Cloud
+  - Reference
+  - API
+---
+
+To be scribbled Compute
--- products/compute/sidebars.js
+++ products/compute/sidebars.js
@@ -1,12 +1,53 @@
+const computeVersions = require("./api/versions.json");
+const {
+  versionSelector,
+  versionCrumb,
+} = require("docusaurus-plugin-openapi-docs/lib/sidebars/utils");
+
 module.exports = {
-  computeapi: [
+  compute_2212: [
     {
-      type: "category",
-      label: "Compute Self-hosted",
-      collapsed: true,
-      items: [
-        require("api/sidebar")
-      ],
+      type: "html",
+      defaultStyle: true,
+      value: versionSelector(computeVersions),
+      className: "version-button",
     },
+    {
+      type: "html",
+      defaultStyle: true,
+      value: versionCrumb(`22-12`),
+    },
+    "compute/api/compute-api-reference-home",
+    require("./api/sidebar"),
+  ],
+  compute_2201: [
+    {
+      type: "html",
+      defaultStyle: true,
+      value: versionSelector(computeVersions),
+      className: "version-button",
+    },
+    {
+      type: "html",
+      defaultStyle: true,
+      value: versionCrumb(`22-01`),
+    },
+    "compute/api/compute-api-reference-home",
+    require("./api/22-01/sidebar"),
+  ],
+  compute_2206: [
+    {
+      type: "html",
+      defaultStyle: true,
+      value: versionSelector(computeVersions),
+      className: "version-button",
+    },
+    {
+      type: "html",
+      defaultStyle: true,
+      value: versionCrumb(`22-06`),
+    },
+    "compute/api/compute-api-reference-home",
+    require("./api/22-06/sidebar"),
   ],
 };
