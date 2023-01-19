b8e61fb843d43823e98c002deb345cbc2de038bd
blindaa121 <blindaa121@gmail.com>
2023-01-19 13:30:57 -0800

    Remove duplicate Slider component

--- src/components/ProductLandingPage/Hero/Hero.js
+++ src/components/ProductLandingPage/Hero/Hero.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 // components
-import Link from '@docusaurus/Link'
-import Slider from '../Slider/Slider'
-import Card from '../Cards/Card'
+import Link from "@docusaurus/Link";
+import Slider from "../../Slider/Slider";
+import Card from "../Cards/Card";
 // styles
-import './Hero.scss'
+import "./Hero.scss";
 
 function ProductHero({ anchorLink, product, header, subheader, sliderCards }) {
   return (
@@ -16,28 +16,28 @@ function ProductHero({ anchorLink, product, header, subheader, sliderCards }) {
             {subheader}
           </header>
           <Link to={anchorLink}>
-            <button className="button button--primary">
-              Learn More
-            </button>
+            <button className="button button--primary">Learn More</button>
           </Link>
         </div>
-        {sliderCards.length && <div className="product-hero__inner-right">
-          <Slider className="product-hero__slider-container">
-            {sliderCards.map((card, i) =>
-              <Card
-                key={i}
-                cta={card.cta}
-                description={card.description}
-                image={card.image}
-                title={card.title}
-                type="imageCard"
-              />
-            )}
-          </Slider>
-        </div>}
+        {sliderCards.length && (
+          <div className="product-hero__inner-right">
+            <Slider className="product-hero__slider-container">
+              {sliderCards.map((card, i) => (
+                <Card
+                  key={i}
+                  cta={card.cta}
+                  description={card.description}
+                  image={card.image}
+                  title={card.title}
+                  type="imageCard"
+                />
+              ))}
+            </Slider>
+          </div>
+        )}
       </div>
     </section>
-  )
+  );
 }
 
-export default ProductHero
+export default ProductHero;
