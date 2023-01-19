b94b0f8b5e8b44a7203582e6d1753da22bc40c12
blindaa121 <blindaa121@gmail.com>
2023-01-19 14:16:01 -0800

    Refactor custom arrow components to resolve console errors

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,6 +4,35 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
 function SliderComponent(props) {
+  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+    <button
+      {...props}
+      className={
+        "slick-prev slick-arrow" + (currentSlide === 0 ? " slick-disabled" : "")
+      }
+      aria-hidden="true"
+      aria-disabled={currentSlide === 0 ? true : false}
+      type="button"
+    >
+      <img src="/icons/slider-arrow-back.svg" />
+    </button>
+  );
+
+  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+    <button
+      {...props}
+      className={
+        "slick-next slick-arrow" +
+        (currentSlide === slideCount - 1 ? " slick-disabled" : "")
+      }
+      aria-hidden="true"
+      aria-disabled={currentSlide === slideCount - 1 ? true : false}
+      type="button"
+    >
+      <img src="/icons/slider-arrow-forward.svg" />
+    </button>
+  );
+
   const defaultSettings = {
     autoplay: true,
     autoplaySpeed: 7500,
@@ -12,8 +41,8 @@ function SliderComponent(props) {
     speed: 850,
     slidesToShow: 2,
     slidesToScroll: 2,
-    nextArrow: <img src="/icons/slider-arrow-forward.svg" />,
-    prevArrow: <img src="/icons/slider-arrow-back.svg" />,
+    nextArrow: <SlickArrowRight />,
+    prevArrow: <SlickArrowLeft />,
     responsive: [
       {
         breakpoint: 996,
