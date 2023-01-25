dcfc43e3bca8915b66148b0bfdfc33a68c2d9a27
blindaa121 <blindaa121@gmail.com>
2023-01-25 10:42:15 -0800

    Remove original slick arrows

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import "./Slider.scss";
 
 function SliderComponent(props) {
   const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
