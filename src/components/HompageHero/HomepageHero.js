009d024a7803c7cb88b1259d3f65c9fb774d912a
Bryan Linda <48506502+blindaa121@users.noreply.github.com>
2023-02-28 18:39:03 -0800

    [UI] Landing Page Updates (#283)
    * Change ordering of product cards
    
    * Remove developer docs header
    
    * Reduce hero padding and apply border styling
    
    * Remove unused img src
    
    * Apply aria-label in place of developer docs header

--- src/components/HompageHero/HomepageHero.js
+++ src/components/HompageHero/HomepageHero.js
@@ -3,25 +3,27 @@ import "./HomepageHero.scss";
 
 function HomepageHero() {
   return (
-    <div className="hero-container container">
-      <header className="hero-content-container">
-        <div className="hero-header-container">
-          <h1 className="hero-header">
-            The home of{" "}
-            <span className="hero-colored-text">developer docs </span> at
-          </h1>
-        </div>
-        <div className="hero-image-container">
-          <img
-            className="hero-image__logo"
-            src="img/PANW_Parent_Logo_Black.svg"
-          />
-        </div>
-      </header>
-      <img
-        className="hero-image__illustration"
-        src="img/homepage-hero/hero-1.svg"
-      />
+    <div className="hero-background-wrapper">
+      <section className="hero-container container">
+        <header className="hero-content-container">
+          <div className="hero-header-container">
+            <h1 className="hero-header">
+              The home of{" "}
+              <span className="hero-colored-text">developer docs </span> at
+            </h1>
+          </div>
+          <div className="hero-image-container">
+            <img
+              className="hero-image__logo"
+              src="img/PANW_Parent_Logo_Black.svg"
+            />
+          </div>
+        </header>
+        <img
+          className="hero-image__illustration"
+          src="img/homepage-hero/hero-1.svg"
+        />
+      </section>
     </div>
   );
 }
