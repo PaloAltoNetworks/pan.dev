0700f8649219c5a9df83eca399694ac9a3e06c51
ssugandh <ssugandh@paloaltonetworks.com>
2023-04-19 10:54:52 +0530

    Updates for Maxwell and Lagrange Update 4

--- products/compute/sidebars.js
+++ products/compute/sidebars.js
@@ -5,6 +5,24 @@ const {
 } = require("docusaurus-plugin-openapi-docs/lib/sidebars/utils");
 
 module.exports = {
+  compute_3000: [
+    {
+      type: "html",
+      defaultStyle: true,
+      value: versionSelector(computeVersions),
+      className: "version-button",
+    },
+    {
+      type: "html",
+      defaultStyle: true,
+      value: versionCrumb(`30-00`),
+    },
+    "compute/api/compute-api-reference-home",
+    "compute/api/access-api-self-hosted",
+    "compute/api/set-up-console",
+    "compute/api/stable-endpoints",
+    require("./api/sidebar"),
+  ],
   compute_2212: [
     {
       type: "html",
@@ -17,11 +35,11 @@ module.exports = {
       defaultStyle: true,
       value: versionCrumb(`22-12`),
     },
-    "compute/api/compute-api-reference-home",
-    "compute/api/access-api-self-hosted",
-    "compute/api/set-up-console",
-    "compute/api/stable-endpoints",
-    require("./api/sidebar"),
+    "compute/api/22-12/compute-api-reference-home",
+    "compute/api/22-12/access-api-self-hosted",
+    "compute/api/22-12/set-up-console",
+    "compute/api/22-12/stable-endpoints",
+    require("./api/22-12/sidebar"),
   ],
   compute_2206: [
     {
@@ -40,23 +58,5 @@ module.exports = {
     "compute/api/22-06/set-up-console",
     "compute/api/22-06/stable-endpoints",
     require("./api/22-06/sidebar"),
-  ],
-  compute_2201: [
-    {
-      type: "html",
-      defaultStyle: true,
-      value: versionSelector(computeVersions),
-      className: "version-button",
-    },
-    {
-      type: "html",
-      defaultStyle: true,
-      value: versionCrumb(`22-01`),
-    },
-    "compute/api/22-01/compute-api-reference-home",
-    "compute/api/22-01/access-api-self-hosted",
-    "compute/api/22-01/set-up-console",
-    "compute/api/22-01/stable-endpoints",
-    require("./api/22-01/sidebar"),
   ],
 };
