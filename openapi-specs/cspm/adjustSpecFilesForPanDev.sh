b8521a523abd45c8e1e4281c3ad01c923b0ac436
AbiMano4688 <abinaya.yam@gmail.com>
2023-10-09 13:36:34 +0530

    Added Collection APIs

--- openapi-specs/cspm/adjustSpecFilesForPanDev.sh
+++ openapi-specs/cspm/adjustSpecFilesForPanDev.sh
@@ -6,7 +6,7 @@ if [ -f "Anomalies.json" ]; then
     jq '.tags |= [{"name":"Anomalies", "description":"temporary description - will be overwritten anyway!"}] | .paths[][].tags[] = "Anomalies"' Anomalies.json > "$tmp" && mv "$tmp" Anomalies.json
 
     # TEMPORARILY change the operationId getPolicies -> get-policies-anomalies
-    sed -i "" "s/getPolicies/get-policies-anomalies/g" Anomalies.json
+    sed -i  "s/getPolicies/get-policies-anomalies/g" Anomalies.json
 fi
 
 # replace the global tags in the IAM file
@@ -26,30 +26,33 @@ if [ -f "IAMIdp.json" ]; then
 fi
 
 for file in *.json; do
-    sed -i "" "s/\/api\/cloud\/cspm\/iam#operation\/get-permissions-access/\/prisma-cloud\/api\/cspm\/get-permissions-access-with-post/g" $file
-    sed -i "" "s/\/api\/cloud\/cspm\/iam#operation\/get-permissions/\/prisma-cloud\/api\/cspm\/get-permissions-with-post/g" $file
+    sed -i  "s/\/api\/cloud\/cspm\/iam#operation\/get-permissions-access/\/prisma-cloud\/api\/cspm\/get-permissions-access-with-post/g" $file
+    sed -i  "s/\/api\/cloud\/cspm\/iam#operation\/get-permissions/\/prisma-cloud\/api\/cspm\/get-permissions-with-post/g" $file
 
-    sed -i "" "s/\/api\/cloud\/cspm\/.*#operation\//\/prisma-cloud\/api\/cspm\//g" $file
+    sed -i  "s/\/api\/cloud\/cspm\/.*#operation\//\/prisma-cloud\/api\/cspm\//g" $file
 
     # https://prisma.pan.dev/docs/cloud/cspm -> /prisma-cloud/docs/cspm/cspm-gs/#use-curl-to-generate-jwt-tokens-in-prisma-cloud
-    sed -i "" "s/https:\/\/prisma.pan.dev\/docs\/cloud/\/prisma-cloud\/docs/g" $file
+    sed -i  "s/https:\/\/prisma.pan.dev\/docs\/cloud/\/prisma-cloud\/docs/g" $file
 
     # turn to relative links
-    sed -i "" "s/https:\/\/prisma.pan.dev/\//g" $file
+    sed -i  "s/https:\/\/prisma.pan.dev/\//g" $file
 
     # /api/cloud/cspm/
-    sed -i "" "s/\/api\/cloud\/cspm/\/prisma-cloud\/api\/cspm/g" $file
+    sed -i  "s/\/api\/cloud\/cspm/\/prisma-cloud\/api\/cspm/g" $file
 
     # /api/cloud -> /prisma-cloud/api/cspm
-    sed -i "" "s/\/api\/cloud/\/prisma-cloud\/api\/cspm/g" $file
+    sed -i  "s/\/api\/cloud/\/prisma-cloud\/api\/cspm/g" $file
 
     # add hyphen (i.e. v2 -> v-2)
-    sed -i "" "s/\/get-asset-inventory-v2-dashboard-filter-options/\/get-asset-inventory-v-2-dashboard-filter-options/g" $file
+    sed -i  "s/\/get-asset-inventory-v2-dashboard-filter-options/\/get-asset-inventory-v-2-dashboard-filter-options/g" $file
 
     # rewrite the GLOBAL tag description
     tmp=$(mktemp)
-    jq '.info.description as $tag_desc | .tags[]?.description |= $tag_desc' $file | \
+     jq '.info.description as $tag_desc| if($tag_desc!=null) then .tags[]?.description |= $tag_desc else . end' $file | \
     
+    # remove S2S headers or parameters if any
+    jq '.paths |= del(.[][].parameters[]? | select(.description!=null ) | select (.description | contains("S2S")))' | \
+
     # delete code snippets
     jq '.paths |= del(.[][]."x-codeSamples")' | \
 
@@ -80,8 +83,11 @@ for file in *.json; do
       }}' | \
 
     # add security field to every endpoint
-    jq '.paths[][].security |= [ { "x-redlock-auth": [] } ]'  > "$tmp" && mv "$tmp" $file
+    jq '.paths[][].security |= [ { "x-redlock-auth": [] } ]'  > "$tmp" && mv "$tmp" $file 
+   
 done
+
+
 
 # app-login endpoint isn't supposed to be protected
 tmp=$(mktemp)
