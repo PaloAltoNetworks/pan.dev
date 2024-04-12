4837c0952b2673c46c2f6d9e3cfc81a9e950fe9f
smitapaloalto <156162707+smitapaloalto@users.noreply.github.com>
2024-04-12 11:24:45 +0530

    build_time_reduction_changes

--- products/compute/sidebars.js
+++ products/compute/sidebars.js
@@ -59,6 +59,42 @@ module.exports = {
     "compute/api/30-03/stable-endpoints",
     require("./api/30-03/sidebar"),
   ],
+  compute_32: [
+    {
+      type: "category",
+      label: "Prisma Cloud Compute Edition - 32.00",
+      collapsed: true,
+      items: [
+        "compute/api/32-00/stable-endpoints",
+        require("./api/32-00/sidebar"),
+      ],
+    },
+  ],
+  compute_3201: [
+    {
+      type: "category",
+      label: "Prisma Cloud Compute Edition - 32.01",
+      collapsed: true,
+      items: [
+        "compute/api/32-01/stable-endpoints",
+        require("./api/32-01/sidebar"),
+      ],
+    },
+  ],
+  compute_3202: [
+    {
+      type: "category",
+      label: "Prisma Cloud Compute Edition - 32.02",
+      collapsed: true,
+      items: [
+        "compute/api/32-02/compute-api-reference-home",
+        "compute/api/32-02/access-api-self-hosted",
+        "compute/api/32-02/set-up-console",
+        "compute/api/32-02/stable-endpoints",
+        require("./api/32-02/sidebar"),
+      ],
+    },
+  ],
   compute_3203: [
     {
       type: "category",
