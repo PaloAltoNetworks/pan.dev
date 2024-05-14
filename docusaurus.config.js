bfa5e9823650df2d6c651ad4a59e0976e9a8dfcc
AbiMano4688 <abinaya.yam@gmail.com>
2024-05-14 16:15:54 +0530

    try me option enable

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -765,7 +765,7 @@ const config = {
             specPath: "openapi-specs/cspm",
             outputDir: "products/prisma-cloud/api/cspm",
             sidebarOptions: { groupPathsBy: "tag", categoryLinkSource: "tag" },
-            hideSendButton: true,
+            hideSendButton: false,
           },
           code: {
             specPath: "openapi-specs/code",
