107a1d677dbc48d370788374d0aff6cc89a085d4
sra <sra@paloaltonetworks.com>
2024-05-30 21:27:43 +0530

    DOCS-5656 Ignore

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -665,7 +665,7 @@ const config = {
             sidebarOptions: { groupPathsBy: "tag" },
             version: "3.0",
             label: "v3.0",
-            baseUrl: "/access/api/insights/",
+            baseUrl: "/access/api/insights/3.0/",
 
             specPath: "openapi-specs/access/insights/2.0",
             outputDir: "products/access/api/insights/",
