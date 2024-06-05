9d00e653352afcf818bb63529506fc43832f4c5c
AbiMano4688 <akubendhiran@paloaltonetworks.com>
2024-06-05 22:51:52 +0530

    Update docusaurus.config.js
    Signed-off-by: AbiMano4688 <akubendhiran@paloaltonetworks.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -764,9 +764,7 @@ const config = {
           cspm: {
             specPath: "openapi-specs/cspm",
             outputDir: "products/prisma-cloud/api/cspm",
-            sidebarOptions: { groupPathsBy: "tag", categoryLinkSource: "tag" },
-            hideSendButton: false,
-            proxy: "https://cors.pan.dev",
+            sidebarOptions: { groupPathsBy: "tag", categoryLinkSource: "tag" }
           },
           code: {
             specPath: "openapi-specs/code",
