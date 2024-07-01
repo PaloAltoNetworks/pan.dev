a74b856c65b762cbc84fb702f702bbbdc1b92fb7
AbiMano4688 <akubendhiran@paloaltonetworks.com>
2024-07-01 17:29:02 +0530

    Hide send API request button
    Signed-off-by: AbiMano4688 <akubendhiran@paloaltonetworks.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -765,6 +765,7 @@ const config = {
             specPath: "openapi-specs/cspm",
             outputDir: "products/prisma-cloud/api/cspm",
             sidebarOptions: { groupPathsBy: "tag", categoryLinkSource: "tag" },
+            hideSendButton: true,
           },
           code: {
             specPath: "openapi-specs/code",
