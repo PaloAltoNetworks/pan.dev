757fb8cc3b3eab1af1122ae64215806d7ab16944
Steven Serrata <sserrata@paloaltonetworks.com>
2024-09-26 12:56:20 -0400

    update gitignore

--- websites/pan-dev/build/assets/js/296.8da307bc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(self.webpackChunkpan_dev = self.webpackChunkpan_dev || []).push([
-  [296],
-  {
-    23296: (e, p, s) => {
-      s.r(p);
-    },
-  },
-]);
--- websites/pan-dev/build/assets/js/985.e65b4f6f.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-(self.webpackChunkpan_dev = self.webpackChunkpan_dev || []).push([
-  [985],
-  {
-    72366: (e, p, s) => {
-      s.r(p);
-    },
-  },
-]);
