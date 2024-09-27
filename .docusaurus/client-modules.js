9c75a9ddeb8e22eb7dd1a5389ef7078384150ffd
Steven Serrata <sserrata@paloaltonetworks.com>
2024-09-26 20:10:16 -0400

    cleanup root level .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/Users/sserrata/projects/panw/pan.dev/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/sserrata/projects/panw/pan.dev/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/sserrata/projects/panw/pan.dev/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/sserrata/projects/panw/pan.dev/src/css/custom.scss'),
-  require('/Users/sserrata/projects/panw/pan.dev/node_modules/docusaurus-theme-openapi-docs/lib/theme/styles.scss'),
-];
