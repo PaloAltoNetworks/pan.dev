6ac41ca7e1815f2f761bb5d90cfd9a900d1ef608
Bryan <blindaa121@gmail.com>
2024-10-02 15:08:28 -0700

    Add nested structure to SCM card and style cleanup

--- src/pages/scm/SCMCard.jsx
+++ src/pages/scm/SCMCard.jsx
@@ -2,35 +2,72 @@ import React from "react";
 import clsx from "clsx";
 import NavbarNavLink from "@theme/NavbarItem/NavbarNavLink";
 
-function SCMCard({ label, description, docs, colorclass }) {
+function SCMCard({ label, description, docs, colorclass, type }) {
   function SCMCardContent({ docs }) {
+    const renderCardContent = () => {
+      if (type && type === "hierarchy") {
+        return Object.entries(docs).map(([category, docs]) => {
+          return (
+            <div>
+              <h3 className="featured-card__product-group-label scm">
+                {category}
+              </h3>
+              {docs.map((doc, i) => {
+                const { label, to, icon } = doc;
+                const iconClass = icon === "doc" ? "doc-icon" : "api-doc-icon";
+
+                return (
+                  <li
+                    className={`featured-card__product-list-item ${iconClass}`}
+                    key={i}
+                  >
+                    <NavbarNavLink
+                      className="featured-card__product-link"
+                      label={label}
+                      to={to}
+                    />
+                  </li>
+                );
+              })}
+            </div>
+          );
+        });
+      } else {
+        return docs.map((doc, i) => {
+          const { label, to, icon } = doc;
+          const iconClass = icon === "doc" ? "doc-icon" : "api-doc-icon";
+
+          return (
+            <li
+              className={`featured-card__product-list-item ${iconClass}`}
+              key={i}
+            >
+              <NavbarNavLink
+                className="featured-card__product-link"
+                label={label}
+                to={to}
+              />
+            </li>
+          );
+        });
+      }
+    };
+
     return (
       <div className="scm-card-container">
-        <ul className="scm-content-list">
-          {docs.map((docs, i) => {
-            const { label, to, icon } = docs;
-            const iconClass = icon === "doc" ? "doc-icon" : "api-doc-icon";
-
-            return (
-              <li
-                className={`featured-card__product-list-item ${iconClass}`}
-                key={i}
-              >
-                <NavbarNavLink
-                  className="featured-card__product-link"
-                  label={label}
-                  to={to}
-                />
-              </li>
-            );
+        <ul
+          className={clsx("scm-content-list", {
+            hierarchy: type === "hierarchy",
           })}
+        >
+          {renderCardContent()}
         </ul>
       </div>
     );
   }
 
   return (
-    <div className={clsx("featured-card-container scm", colorclass)}>
+    <div className={clsx("featured-card-container scm-landing", colorclass)}>
       <div className="featured-card-content">
         <h2 className="featured-card-content__label">{label}</h2>
         <div className="featured-card-content__section-divider" />
