1cc624e42e1fc3bd3dcc38aab49046d0abd597ce
Bryan <blindaa121@gmail.com>
2025-02-07 15:06:23 -0800

    Remove console log

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./styles.css";
 
 const Tooltip = ({ children, text, isVisible }) => {
-  console.log({ isVisible });
   return (
     <div className="tooltip-container">
       {children}
