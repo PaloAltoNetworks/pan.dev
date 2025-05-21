663fca663f34770dda3ab81a96c7201d9bee72a8
Steven Serrata <sserrata@paloaltonetworks.com>
2025-05-21 10:56:20 -0500

    fix: update styles.module.css import paths after moving file to airs folder

--- src/components/UseCasesTabs.js
+++ src/components/UseCasesTabs.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styles from "../pages/styles.module.css";
+import styles from "../pages/airs/styles.module.css";
 import { useColorMode } from "@docusaurus/theme-common";
 
 const useCases = [
