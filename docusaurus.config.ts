d61da6896e2baaa0326ed00f7e7a6dd6732aaf61
Steven Serrata <9343811+sserrata@users.noreply.github.com>
2025-07-01 06:37:44 -0500

    upgrade docusaurus to v3.8.1 and plugin to v4.5.1 (#950)
    * upgrade docusaurus to v3.8.1 and plugin to v4.5.1
    
    * Fix @import statement warning
    
    ---------
    
    Co-authored-by: Bryan <blindaa121@gmail.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,6 +22,7 @@ if (process.env.CI_MERGE_REQUEST_IID) {
 const config = {
   future: {
     experimental_faster: (process.env.DOCUSAURUS_FASTER ?? "true") === "true",
+    v4: true
   },
   title: "Develop with Palo Alto Networks",
   tagline:
