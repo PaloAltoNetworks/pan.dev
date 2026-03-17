e3e77d53d59a6484a1626dbc49c401edf43f3bb5
sra <sra@paloaltonetworks.com>
2026-03-17 22:54:21 +0530

    Ignore swap file

--- src/pages/strata-cloud-manager/index.js
+++ src/pages/strata-cloud-manager/index.js
@@ -185,7 +185,7 @@ export default function SCMLandingPage() {
             icon: "api-doc",
           },
         ],
-        Posture: [
+        "Posture": [
           {
             to: "scm/api/config/posture-management/introduction-posture",
             label: "Posture",
@@ -230,7 +230,7 @@ export default function SCMLandingPage() {
           },
         ],
       },
-    },
+    },  
     {
       label: "Monitoring",
       description: "",
