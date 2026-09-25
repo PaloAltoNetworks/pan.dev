1064a8753e9e7402bdbf7c0f8fb823b021af0a8a
Offer Azoury <oazoury@paloaltonetworks.com>
2026-09-25 14:24:03 +0300

    WIP: safety snapshot before Cursor tenant migration 2026-09-25

---
+++ src/theme/NavbarItem/ComponentTypes.tsx
@@ -0,0 +1,7 @@
+import ComponentTypes from "@theme-original/NavbarItem/ComponentTypes";
+import ProductMenu from "@site/src/components/ProductMenu";
+
+export default {
+  ...ComponentTypes,
+  "custom-productMenu": ProductMenu,
+};
